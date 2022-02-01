c6b8259618df3142239f6d405ce736ebae8cce65
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-02-01 13:13:09 -0800

    commit

--- server.py
+++ server.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route("/")
-def home():
-  return "Hello World! <h1>Hello<h1>"
+@app.route('/')
 
-if __name__ == "__main__":
-  app.run()
+def hello_world():
+    return 'Hello World!'
+
+if __name__=="__main__":
+
+    app.run(debug=True)
