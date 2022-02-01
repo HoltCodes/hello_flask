d795cc950d63007e8be44cf675479759719df760
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-02-01 14:58:30 -0800

    commit

--- understanding_routing.py
+++ understanding_routing.py
@@ -10,20 +10,35 @@ def hello_world():
 def Dojo():
     return 'Dojo'
 
-@app.route('/say/<Flask>')
-def say(Flask):
-  print(Flask)
-  return "Hi " + Flask
+# @app.route('/say/<Flask>')
+# def say(Flask):
+#   print(Flask)
+#   return "Hi " + Flask
 
-@app.route('/say/<Michael>')
-def say(Michael):
-  print(Michael)
-  return "Hi " + Michael  
+# @app.route('/say/<Michael>')
+# def say(Michael):
+#   print(Michael)
+#   return "Hi " + Michael  
 
-@app.route('/say/<John>')
-def say(John):
-  print(John)
-  return "Hi " + John
+# @app.route('/say/<John>')
+# def say(John):
+#   print(John)
+#   return "Hi " + John
+
+@app.route('/say/<name>')
+def say_name(name):
+    return f"Hi {name.capitalize()}!"
+
+@app.route('/repeat/<int:num>/<string:word>')
+def repeat_word(num, word):
+    output = ''
+
+    for i in range(0,num):
+        output += f"<p>{word}</p>"
+
+    return output    
+
+@app.route('/repeat')
 
 if __name__=="__main__":
-    app.run(debug=True)
+  app.run(debug=True)
