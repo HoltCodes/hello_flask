f0c6a85f1fbb86ebfdf6430b594f3a0dd1950cf9
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-02-05 12:46:27 -0800

    commit

--- Checkerboard/server.py
+++ Checkerboard/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", row=8,col=8,color_one='black', color_two='orange')
 
 if __name__=="__main__":
     app.run(debug=True)
