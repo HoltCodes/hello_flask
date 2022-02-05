35ea045e8c437a8d8c5302582640a21ff0a3587e
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-02-05 13:26:10 -0800

    commit

--- Checkerboard/server.py
+++ Checkerboard/server.py
@@ -1,10 +1,11 @@
+from turtle import color
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", row=8,col=8,color_one='black', color_two= 'orange')
 
 if __name__=="__main__":
     app.run(debug=True)
