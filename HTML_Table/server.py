df02c0efe36ca046e0ee7236978cd29adfe9e5db
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-02-05 15:28:30 -0800

    commit

--- HTML_Table/server.py
+++ HTML_Table/server.py
@@ -10,7 +10,9 @@ def index():
         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
         {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-    return render_template()
+    return render_template("index.html",users=users)
+
+
 
 if __name__=="__main__":
-  app.run(debug=True)
+    app.run(debug=True)
