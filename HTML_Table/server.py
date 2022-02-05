8e739dc0f7f4c01ee460a3e66fae0f5e65066e9f
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-02-05 15:03:18 -0800

    commit

--- HTML_Table/server.py
+++ HTML_Table/server.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
     return render_template()
 
 if __name__=="__main__":
