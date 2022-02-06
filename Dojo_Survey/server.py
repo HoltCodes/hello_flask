b46b409f84488d52ba24b8eac7e5f4e792d111cc
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-02-05 17:35:26 -0800

    commit

--- Dojo_Survey/server.py
+++ Dojo_Survey/server.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, request
 
 app = Flask(__name__)
 
+app.secret_key="Holt.Codes"
+
 @app.route('/')
 def index():
-   
     return render_template("index.html")
 
+@app.route('/success')
+def success():
+    return render_template('success.html')    
+
 if __name__=="__main__":
-    app.run(debug=True)
+  app.run(debug=True)
