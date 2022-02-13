269e3b64f192c4594a52178916c674dc6b2abb35
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-02-13 09:25:42 -0400

    commit

--- Dojo_and_Ninjas/flask_app/controllers/dojos.py
+++ Dojo_and_Ninjas/flask_app/controllers/dojos.py
@@ -1,15 +1,18 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, request
 from flask_app import app
 from flask_app.models.dojo import Dojo
 
+
 @app.route('/')
 def index():
-  return redirect('/dojos')
+    return redirect('/dojos')
 
 @app.route('/dojos')
 def dojos():
-  dojo = Dojo.get_all()
-  return render_template('index.html',all_dojos = dojos)
+    dojos = Dojo.get_all()
+    return render_template("index.html",all_dojos = dojos)
+
+
 @app.route('/create/dojo',methods=['POST'])
 def create_dojo():
     Dojo.save(request.form)
