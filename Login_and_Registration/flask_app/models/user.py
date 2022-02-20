075a9882302bd6ba01b060d0933ab0dc499d7cb7
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-02-19 20:47:07 -0400

    commit

--- Login_and_Registration/flask_app/models/user.py
+++ Login_and_Registration/flask_app/models/user.py
@@ -1,44 +1,47 @@
 from flask_app.config.mysqlconnection import connectToMySQL
-import re
+import re	# the regex module
+# create a regular expression object that we'll use later   
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 from flask import flash
 
 class User:
-  db = "login_and_registration"
-  def __init__(self,data):
-    self.id = data['id']
-    self.first_name = data['first_name']
-    self.last_name = data['last_name']
-    self.email = data['email']
-    self.password = data['password']
-    self.created_at = data['create_at']
-    self.updated_at = data['updated_at']
+    db = "login_and_registration"
+    def __init__(self,data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
 
     @classmethod
     def save(cls,data):
-      query = "INSERT INTO users (first_name,last_name,email,password) VALUES(%(first_name)s,%(last_name)s,%(email)s%(password)s)"
-      return connectToMySQL(cls.db).query_db(query.data)
+        query = "INSERT INTO users (first_name,last_name,email,password) VALUES(%(first_name)s,%(last_name)s,%(email)s,%(password)s)"
+        return connectToMySQL(cls.db).query_db(query,data)
 
     @classmethod
     def get_all(cls):
-      query = "SELECT * FROM users;"
-      results = connectToMySQL(cls.db).query_db(query)
-      users = []
-      for row in results:  
+        query = "SELECT * FROM users;"
+        results = connectToMySQL(cls.db).query_db(query)
+        users = []
+        for row in results:
+            users.append( cls(row))
+        return users
 
     @classmethod
     def get_by_email(cls,data):
-      query = "SELECT * FROM users WHERE email = %(email)s;"    
-      results = connectToMySQL(cls.db).query_db(query,data)
-      if len(results) < 1:
-        return False
-      return cls(results[0])
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        results = connectToMySQL(cls.db).query_db(query,data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
 
     @classmethod
     def get_by_id(cls,data):
-      query = "SELECT * FROM users WHERE id = %(id)s;"    
-      results = connectToMySQL(cls.db).query_db(query,data)
-      return cls(results[0])
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        results = connectToMySQL(cls.db).query_db(query,data)
+        return cls(results[0])
 
     @staticmethod
     def validate_register(user):
@@ -62,4 +65,4 @@ class User:
             is_valid= False
         if user['password'] != user['confirm']:
             flash("Passwords don't match","register")
-        return is_valid  
+        return is_valid
